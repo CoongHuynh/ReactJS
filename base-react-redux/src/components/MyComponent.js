f5db13f8c463219ee323c4b448a4f496f0a78d9c
CoongHuynh <huynhcong.dev@gmail.com>
2024-07-04 21:32:11 +0700

    22 nesting Component - Component cha lồng con

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -1,66 +1,11 @@
 import React from "react";
-
+import UserInfor from "./UserInfor";
 class MyComponent extends React.Component {
-  state = {
-    name: "Huynh",
-    address: "hoi dan IT",
-    age: 24,
-  };
-
-  handleClick(event) {
-    console.log(" click me my button");
-
-    //merge state => react class
-    this.setState({
-      name: "Cong Huynh",
-      age: Math.floor(Math.random() * 100 + 1),
-    });
-
-    // this.setState({
-    //   age: Math.floor(Math.random() * 100 + 1),
-    // });
-  }
-  handleOnMouseOver(event) {
-    console.log(event);
-  }
-
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
   //JSX
   render() {
     return (
       <div>
-        My name is {this.state.name} and i'm from {this.state.address}
-        <button onMouseOver={this.handleOnMouseOver}> Hover me </button>
-        <button
-          onClick={(event) => {
-            this.handleClick(event);
-          }}
-        >
-          {" "}
-          click{" "}
-        </button>
-        <form
-          onSubmit={(event) => {
-            this.handleOnSubmit(event);
-          }}
-        >
-          <input
-            type="text"
-            onChange={(event) => {
-              this.handleOnChangeInput(event);
-            }}
-          />
-          <button>Submit</button>
-        </form>
+        <UserInfor />
       </div>
     );
   }
