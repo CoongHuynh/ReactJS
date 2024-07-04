d892fb117f350c3bf92cbd3f29a30a23fe7411cb
CoongHuynh <huynhcong.dev@gmail.com>
2024-07-04 20:40:23 +0700

    19 DOM event

--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -7,11 +7,20 @@ class MyComponent extends React.Component {
     age: 24,
   };
 
+  handleClick(event) {
+    console.log(" click me my button");
+    console.log("my name is: ", this.state.name);
+  }
+  handleOnMouseOver(event) {
+    console.log(event);
+  }
   //JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and i'm from {this.state.address}
+        <button onMouseOver={this.handleOnMouseOver}> Hover me </button>
+        <button onClick={this.handleClick}> click </button>
       </div>
     );
   }
