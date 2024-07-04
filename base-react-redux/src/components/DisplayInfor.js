4192c24e047d86afea7d43f576d5e403a0f10712
CoongHuynh <huynhcong.dev@gmail.com>
2024-07-04 22:32:50 +0700

    25 conditional Output - Câu điều kiện, thêm chức năng ẩn hiện

--- base-react-redux/src/components/DisplayInfor.js
+++ base-react-redux/src/components/DisplayInfor.js
@@ -1,21 +1,42 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = { isShowListUser: true };
+
+  handleShowHide = () => {
+    this.setState({ isShowListUser: !this.state.isShowListUser });
+  };
+
   // destructuring array/object
   render() {
     const { listUsers } = this.props;
 
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>My name {user.name}</div>
-              <div>My age: {user.age}</div>
-              <hr />
-            </div>
-          );
-        })}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowListUser === true
+              ? "Hide list user:"
+              : "Hide list user:"}
+          </span>
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div>My name {user.name}</div>
+                  <div>My age: {user.age}</div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- base-react-redux/src/components/MyComponent.js
+++ base-react-redux/src/components/MyComponent.js
@@ -4,7 +4,7 @@ import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
   state = {
     listUser: [
-      { id: 1, name: "hoi dan it", age: "24" },
+      { id: 1, name: "hoi dan it", age: "15" },
       { id: 2, name: "Huynh", age: "27" },
       { id: 3, name: "CongHuynh", age: "29" },
     ],
