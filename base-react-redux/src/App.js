06e57dd71a18b06a74fbdbd545f7778e6813cf31
CoongHuynh <huynhcong.dev@gmail.com>
2024-07-08 20:10:37 +0700

    48 Điều hướng trang với Links

--- base-react-redux/src/App.js
+++ base-react-redux/src/App.js
@@ -1,9 +1,21 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+
+      <div>
+        test link
+        <button>
+          <Link to="/admins">Go to admin </Link>
+        </button>
+        <button>
+          <Link to="/users">Go to user</Link>
+        </button>
+      </div>
     </div>
   );
 };
---
+++ base-react-redux/src/components/Admin/Admin.js
@@ -0,0 +1,4 @@
+const Admin = (props) => {
+  return <div>Admin page</div>;
+};
+export default Admin;
