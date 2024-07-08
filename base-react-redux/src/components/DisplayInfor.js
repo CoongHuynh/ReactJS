8bccb2adf165412de08645d630d2ec21045658d4
CoongHuynh <huynhcong.dev@gmail.com>
2024-07-08 16:17:28 +0700

    35 useState Hook Kiểm soát state với function component

--- base-react-redux/src/components/DisplayInfor.js
+++ base-react-redux/src/components/DisplayInfor.js
@@ -1,50 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg";
-// class DisplayInfor extends React.Component {
 
-//   // destructuring array/object
-//   render() {
-//     console.log(">>> Cal me render");
-//     const { listUsers } = this.props;
-
-//     return (
-//       <div className="display-infor-container">
-
-//         {this.state.isShowListUser && (
-//           <>
-//             {listUsers.map((user) => {
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>My name {user.name}</div>
-//                   <div>My age: {user.age}</div>
-//                   <div>
-//                     <button
-//                       onClick={() => {
-//                         this.props.handleDeleteUser(user.id);
-//                       }}
-//                     >
-//                       {" "}
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-//}
+//stateless tức không có state thì có thể sài funtion component
+//statefull: khi funtion cần kiểm soát data (có state) thì cần sử dụng class component
 const DisplayInfor = (props) => {
   const { listUsers } = props;
+  const [isShowHideListuser, setShowHideListUser] = useState(true);
+  // this.state = { isShowHideListuser: true };
+
+  const handleShowHideListUser = () => {
+    // this.setState({ isShowHideListuser: true });
+    setShowHideListUser(!isShowHideListuser);
+  };
 
   return (
     <div className="display-infor-container">
-      {true && (
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListuser === true ? "Hide list users" : "Show list users"}
+        </span>
+      </div>
+      {isShowHideListuser && (
         <>
           {listUsers.map((user) => {
             return (
